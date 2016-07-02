f778c95804e8e5102a7b87e3c6524b101d7c9513
John Leacox <johnlcox@gmail.com>
2016-07-02 13:40:46 -0500

    Try using MethodCall to call setAccessible0

--- accessibleobject-bootstrap/src/main/java/com/leacox/example/bytebuddy/Main.java
+++ accessibleobject-bootstrap/src/main/java/com/leacox/example/bytebuddy/Main.java
@@ -2,17 +2,22 @@ package com.leacox.example.bytebuddy;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.none;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.MethodCall;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Method;
 import java.util.jar.JarFile;
 
@@ -28,12 +33,12 @@ public class Main {
 
     String methodName = "inAccessibleMethod";
 
-    Method method = foo.getClass().getMethod(methodName);
+    Method method = foo.getClass().getDeclaredMethod(methodName);
     method.setAccessible(true);
     method.invoke(foo);
   }
 
-  private static void instrument() {
+  private static void instrument() throws Exception {
     Instrumentation instrumentation = ByteBuddyAgent.install();
 
     File file = new File("../accessibleobject-instrumentation/target/accessibleobject-instrumentation-1.0-SNAPSHOT.jar");
@@ -41,13 +46,23 @@ public class Main {
     System.out.println("jar path: " + path);
     Boolean exists = file.exists();
 
+    JarFile jarFile;
     try {
-      instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(file));
+      jarFile = new JarFile(file);
+      instrumentation.appendToBootstrapClassLoaderSearch(jarFile);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
 
-    TypePool typePool = TypePool.Default.of(Main.class.getClassLoader());
+    TypePool typePool = TypePool.Default.of(new ClassFileLocator.Compound(
+        new ClassFileLocator.ForJarFile(jarFile),
+        ClassFileLocator.ForClassLoader.of(Main.class.getClassLoader())));
+
+    String setAccessible0MethodName = "setAccessible0";
+    Class[] paramTypes = new Class[2];
+    paramTypes[0] = AccessibleObject.class;
+    paramTypes[1] = boolean.class;
+    Method setAccessible0Method = AccessibleObject.class.getDeclaredMethod(setAccessible0MethodName, paramTypes);
 
     new AgentBuilder.Default()
         .disableClassFormatChanges()
@@ -60,10 +75,21 @@ public class Main {
           public DynamicType.Builder<?> transform(
               DynamicType.Builder<?> builder, TypeDescription typeDescription,
               ClassLoader classLoader) {
-            return builder.method(named("setAccessible"))
+            return builder.method(named("setAccessible").and(takesArguments(boolean.class)))
                 .intercept(MethodDelegation.to(
-                    typePool.describe("com.leacox.example.bytebuddy.instrument.java.lang.reflect.SetAccessibleImplementation")
+                    typePool.describe("com.leacox.example.bytebuddy.instrument.java.lang.reflect.SetAccessibleImplementation").resolve()
                 ));
+          }
+        })
+        .type(named("com.leacox.example.bytebuddy.instrument.java.lang.reflect.SetAccessibleImplementation"))
+        .transform(new AgentBuilder.Transformer() {
+          @Override
+          public DynamicType.Builder<?> transform(
+              DynamicType.Builder<?> builder, TypeDescription typeDescription,
+              ClassLoader classLoader) {
+            return builder.method(named("setAccessible1"))
+                .intercept(
+                    MethodCall.invoke(new MethodDescription.ForLoadedMethod(setAccessible0Method)).withAllArguments());
           }
         })
         .installOnByteBuddyAgent();
--- accessibleobject-instrumentation/src/main/java/com/leacox/example/bytebuddy/instrument/java/lang/reflect/SetAccessibleImplementation.java
+++ accessibleobject-instrumentation/src/main/java/com/leacox/example/bytebuddy/instrument/java/lang/reflect/SetAccessibleImplementation.java
@@ -1,10 +1,19 @@
 package com.leacox.example.bytebuddy.instrument.java.lang.reflect;
 
+import net.bytebuddy.implementation.bind.annotation.This;
+
+import java.lang.reflect.AccessibleObject;
+
 /**
  * @author John Leacox
  */
 public class SetAccessibleImplementation {
-  public static void setAccessible(boolean flag) throws SecurityException {
+  public static void setAccessible(@This AccessibleObject ao, boolean flag) throws SecurityException {
     System.out.println("***** We got here *****");
+    setAccessible1(ao, flag);
+  }
+
+  private static void setAccessible1(AccessibleObject ao, boolean flag){
+
   }
 }
